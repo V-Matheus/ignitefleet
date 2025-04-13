62571880e8e9de1fb4383a1acc76976f05f29591
Victor Matheus <victormatheus507@gmail.com>
2025-04-13 11:38:42 -0300

    login with google

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,15 +1,49 @@
 import { Container, Slogan, Title } from './styles';
 import backgroundImg from '../../assets/background.png';
 import { Button } from '../../components/Button';
+import { GoogleSignin } from '@react-native-google-signin/google-signin';
+import { useState } from 'react';
+import { Alert } from 'react-native';
+
+GoogleSignin.configure({
+  scopes: ['email', 'profile'],
+  webClientId: process.env.EXPO_PUBLIC_WEB_CLIENT_ID,
+  iosClientId: process.env.EXPO_PUBLIC_IOS_CLIENT_ID,
+});
 
 export function SignIn() {
+  const [isAuthenticating, setIsAuthenticating] = useState(false);
+
+  async function handleGoogleSignIn() {
+    try {
+      setIsAuthenticating(true);
+
+      const { data } = await GoogleSignin.signIn();
+      
+      if (data?.idToken) {
+        
+      } else {
+        Alert.alert('Entrar', 'Não foi possível conectar com a conta Google');
+      }
+    } catch (error) {
+      console.log(error);
+      Alert.alert('Entrar', 'Não foi possível conectar com a conta Google');
+    } finally {
+      setIsAuthenticating(false);
+    }
+  }
+
   return (
     <Container source={backgroundImg}>
       <Title>Ignite Fleet</Title>
 
       <Slogan>Gestão de uso de veículos</Slogan>
 
-      <Button title="Entrar com o Google" />
+      <Button
+        title="Entrar com o Google"
+        isLoading={isAuthenticating}
+        onPress={handleGoogleSignIn}
+      />
     </Container>
   );
 }
