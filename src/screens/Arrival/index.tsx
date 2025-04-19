a79b14e2c44a129cf1c472deceba45a3bc4cfc46
Victor Matheus <victormatheus507@gmail.com>
2025-04-19 16:13:59 -0300

    create to register arrival screen

--- src/screens/Arrival/index.tsx
+++ src/screens/Arrival/index.tsx
@@ -1,5 +1,14 @@
 import { useRoute } from '@react-navigation/native';
-import { Container } from './styles';
+import {
+  Container,
+  Content,
+  Description,
+  Footer,
+  Label,
+  LicensePlate,
+} from './styles';
+import Header from 'src/components/Header';
+import { Button } from 'src/components/Button';
 
 type RouteParamsProps = {
   id: string;
@@ -9,7 +18,24 @@ export function Arrival() {
   const route = useRoute();
   const { id } = route.params as RouteParamsProps;
 
-  console.log('id =>', id);
+  return (
+    <Container>
+      <Header title="Chegada" />
+      <Content>
+        <Label>Placa do veículo</Label>
+        <LicensePlate>XXX000</LicensePlate>
+        <Label>Finalidade</Label>
+        <Description>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatum
+          ipsum ea expedita veniam recusandae id voluptatibus, commodi,
+          laboriosam fugiat placeat incidunt doloribus hic alias est non numquam
+          aliquid repudiandae nihil?
+        </Description>
 
-  return <Container></Container>;
+        <Footer>
+          <Button title="Registrar Chegada" />
+        </Footer>
+      </Content>
+    </Container>
+  );
 }
