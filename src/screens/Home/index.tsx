f194c31715b679cfbdc4d142d86c763b904aaed6
Victor Matheus <victormatheus507@gmail.com>
2025-04-19 15:49:01 -0300

    filtering vehicle in use

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,13 +1,15 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Container, Content } from './styles';
 import { HomeHeader } from 'src/components/HomeHeader';
 import { CarStatus } from 'src/components/CarStatus';
 import { useNavigation } from '@react-navigation/native';
 import { useQuery } from 'src/libs/realm';
 import { Historic } from 'src/libs/realm/schemas/Historic';
-
+import { Alert } from 'react-native';
 
 export function Home() {
+  const [vehicleInUse, setVehicleInUse] = useState<Historic | null>(null);
+
   const { navigate } = useNavigation();
 
   const historic = useQuery(Historic);
@@ -17,15 +19,31 @@ export function Home() {
   }
 
   function fetchVehicle() {
-    historic
+    try {
+      const vehicle = historic.filtered("status = 'departure'")[0];
+      setVehicleInUse(vehicle);
+    } catch (error) {
+      console.log(error);
+      Alert.alert(
+        'Veículo em uso',
+        'Não foi possível carregar o veículo em uso.',
+      );
+    }
   }
+
+  useEffect(() => {
+    fetchVehicle();
+  }, []);
 
   return (
     <Container>
       <HomeHeader />
 
       <Content>
-        <CarStatus onPress={handleRegisterMovement} />
+        <CarStatus
+          licensePlate={vehicleInUse?.license_plate}
+          onPress={handleRegisterMovement}
+        />
       </Content>
     </Container>
   );
