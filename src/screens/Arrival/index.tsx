f8add50a259c9f0d1bed00e3c23bcd426e31c960
Victor Matheus <victormatheus507@gmail.com>
2025-04-19 16:38:57 -0300

    remove vehicle in use

--- src/screens/Arrival/index.tsx
+++ src/screens/Arrival/index.tsx
@@ -1,4 +1,4 @@
-import { useRoute } from '@react-navigation/native';
+import { useNavigation, useRoute } from '@react-navigation/native';
 import {
   Container,
   Content,
@@ -11,9 +11,10 @@ import Header from 'src/components/Header';
 import { Button } from 'src/components/Button';
 import ButtonIcon from 'src/components/ButtonIcon';
 import { X } from 'phosphor-react-native';
-import { useObject } from 'src/libs/realm';
+import { useObject, useRealm } from 'src/libs/realm';
 import { Historic } from 'src/libs/realm/schemas/Historic';
 import { BSON } from 'realm';
+import { Alert } from 'react-native';
 
 type RouteParamsProps = {
   id: string;
@@ -22,20 +23,43 @@ type RouteParamsProps = {
 export function Arrival() {
   const route = useRoute();
   const { id } = route.params as RouteParamsProps;
+  const { goBack } = useNavigation();
 
   const historic = useObject(Historic, new BSON.UUID(id));
+  const realm = useRealm();
+
+  function handleRemoveVehicleUsage() {
+    Alert.alert('Cancelar', 'Cancelar a utilização do veículo?', [
+      { text: 'Não', style: 'cancel' },
+      { text: 'Sim', onPress: () => removeVehicleUsage() },
+    ]);
+  }
+
+  function removeVehicleUsage() {
+    realm.write(() => {
+      realm.delete(historic);
+    });
+
+    goBack();
+  }
 
   return (
     <Container>
       <Header title="Chegada" />
       <Content>
-        <Label>Placa do veículo</Label>
-        <LicensePlate>{historic?.license_plate}</LicensePlate>
-        <Label>Finalidade</Label>
-        <Description>{historic?.description}</Description>
+        {historic?.isValid() ? (
+          <>
+            <Label>Placa do veículo</Label>
+            <LicensePlate>{historic?.license_plate}</LicensePlate>
+            <Label>Finalidade</Label>
+            <Description>{historic?.description}</Description>
+          </>
+        ) : (
+          <Label>O registro foi removido.</Label>
+        )}
 
         <Footer>
-          <ButtonIcon icon={X} />
+          <ButtonIcon icon={X} onPress={handleRemoveVehicleUsage} />
           <Button title="Registrar Chegada" />
         </Footer>
       </Content>
