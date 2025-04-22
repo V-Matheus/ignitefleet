68a4248b351ced7491137e64763d681ae3002f55
Victor Matheus <victormatheus507@gmail.com>
2025-04-22 16:47:01 -0300

    storing date and time of last synchronization

---
+++ src/libs/asyncStorage/syncStorage.ts
@@ -0,0 +1,19 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+const STORAGE_ASYNC_KEY = '@ignitefleet:last_sync';
+
+export async function saveLastSyncTimestamp() {
+  const timestamp = new Date().getTime();
+  await AsyncStorage.setItem(STORAGE_ASYNC_KEY, timestamp.toString());
+
+  return timestamp;
+}
+
+export async function getLastAsyncTimestamp() {
+  const response = await AsyncStorage.getItem(STORAGE_ASYNC_KEY);
+  if (!response) {
+    return null;
+  }
+
+  return new Number(response);
+}
