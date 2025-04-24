9280273374affe7b4f94008a97d82405b77722f4
Victor Matheus <victormatheus507@gmail.com>
2025-04-24 18:58:11 -0300

    add maps api key

---
+++ app.config.js
@@ -0,0 +1,50 @@
+module.exports = {
+  expo: {
+    name: 'ignitefleet',
+    slug: 'ignitefleet',
+    version: '1.0.0',
+    orientation: 'portrait',
+    icon: './assets/icon.png',
+    userInterfaceStyle: 'dark',
+    newArchEnabled: true,
+    splash: {
+      image: './assets/splash.png',
+      resizeMode: 'cover',
+      backgroundColor: '#202024',
+    },
+    ios: {
+      supportsTablet: true,
+      config: {
+        googleMapsApiKey: process.env.EXPO_PUBLIC_GOOGLE_MAPS_API_KEY,
+      },
+    },
+    android: {
+      adaptiveIcon: {
+        foregroundImage: './assets/adaptive-icon.png',
+        backgroundColor: '#202024',
+      },
+      package: 'com.v_theux.ignitefleet',
+      config: {
+        apiKey: process.env.EXPO_PUBLIC_GOOGLE_MAPS_API_KEY,
+      },
+    },
+    web: {
+      favicon: './assets/favicon.png',
+    },
+    plugins: [
+      'expo-font',
+      [
+        'expo-location',
+        {
+          locationAlwaysAndWhenInUsePermission:
+            'Allow $(PRODUCT_NAME) to use your location.',
+        },
+      ],
+    ],
+    extra: {
+      eas: {
+        projectId: 'e0811872-75fb-44c0-9005-898b450b2204',
+      },
+    },
+  },
+};
